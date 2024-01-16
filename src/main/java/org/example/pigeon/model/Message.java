e4464d0735189ee3952e9d1b809d12212b6d5834
Перминов Денис <dpshar@mail.ru>
2024-01-16 12:09:15 +0300

    Upload images

--- src/main/java/org/example/pigeon/model/Message.java
+++ src/main/java/org/example/pigeon/model/Message.java
@@ -17,6 +17,7 @@ public class Message {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User author;
+    private String filename;
 
     public Message() {
     }
@@ -57,6 +58,14 @@ public class Message {
 
     public void setAuthor(User author) {
         this.author = author;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
     }
 }
 
